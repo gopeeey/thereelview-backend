17a2ffe60b5ed98d563ee438bac8c9b55b9da7c6
Samuel <sammygopeh@gmail.com>
2021-04-21 02:28:23 +0100

    removed dotenv from prod dependencies

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-const dotenv = require('dotenv');
-dotenv.config();
+
 module.exports = {
     dbUrl: process.env.DB_URL || 'mongodb://127.0.0.1:27017/thereelviews',
     port: process.env.PORT || 3030
